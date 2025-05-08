706b380fb12c44c5865bbffa7e0751f7ba90a834
morph <random@gmail.com>
2025-05-08 16:11:42 +0700

    A template almost done

---
+++ main.js
@@ -0,0 +1,20 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark-theme");
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+const faders = document.querySelectorAll('.fade-in-section');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-visible');
+        observer.unobserve(entry.target); // чтобы один раз срабатывало
+    }
+  });
+});
+
+faders.forEach(el => observer.observe(el));

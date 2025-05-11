f2e8620fb10c28a85aff083d6b136a02294ad936
morph <random@gmail.com>
2025-05-11 20:56:53 +0700

    the site getting better every hour

--- public/main.js
+++ public/main.js
@@ -12,9 +12,16 @@ const faders = document.querySelectorAll('.fade-in-section');
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('is-visible');
-        observer.unobserve(entry.target); // чтобы один раз срабатывало
+        // observer.unobserve(entry.target); // чтобы один раз срабатывало
     }
   });
 });
 
 faders.forEach(el => observer.observe(el));
+
+function scrollToIntro() {
+    const target = document.querySelector(".empty-1");
+    if (target) {
+      target.scrollIntoView({behavior: "smooth"})
+    }
+}
